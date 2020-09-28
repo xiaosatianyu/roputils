e13107715bdd60a36d508ec95c645d1a3756b363
xiaosatianyu <xiaosatianyu@126.com>
2020-09-28 22:52:28 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="roputils",
+    version="1",
+    description="Python roputils",
+    author="xiaosa",
+    author_email="xiaosa",
+    platforms=["any"],
+    license="BSD",
+    url="https://github.com/xiaosatianyu/roputils",
+    packages=find_packages(), )
